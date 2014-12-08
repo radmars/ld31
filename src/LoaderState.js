4eaa7ae4de1b09634b45ccc4ee5a2e7c9229d7d3
Eugene Marcotte <emarcotte@gmail.com>
2014-12-07 21:31:57 -0500

    loading text

--- src/LoaderState.js
+++ src/LoaderState.js
@@ -15,38 +15,52 @@ var LoaderState = (function() {
     };
 
     LoaderState.prototype.resize = function(width, height) {
-        this.camera.aspect = width / height;
-        return this.camera.updateProjectionMatrix();
+        this.cx = width / 2;
+        this.cy = height / 2;
+        this.camera.right = width;
+        this.camera.bottom = height;
+        this.camera.updateProjectionMatrix();
     };
 
     LoaderState.prototype.onStart = function(game) {
         this.scene = new THREE.Scene();
+        this.font = new TextRenderer.Font({
+            font: "monospace",
+            size: 32,
+            fgColor: 'white',
+        });
+
         game.renderer.setClearColor(0x2e2e2e, 1);
         game.renderer.autoClear = false;
         game.loader.load(this.nextState.getAssets());
-        this.camera = new THREE.PerspectiveCamera(60,
-            window.innerWidth / window.innerHeight,
-            1,
-            100
-        );
-        this.camera.position.y = -200;
-        this.camera.position.z = 200;
-        return this.camera.lookAt(new THREE.Vector3());
+        this.camera = new THREE.OrthographicCamera( 0, game.width, 0, game.height );
+        this.scene = new THREE.Scene();
+        this.camera.position.z = 10;
+        game.renderer.clear();
+        game.renderer.render(this.scene, this.camera);
+
     };
 
     LoaderState.prototype.onStop = function(game) {
         return game.renderer.autoClear = true;
     };
 
-    LoaderState.prototype.update = function(game, delta) {
+    LoaderState.prototype.update = function(game, dt) {
         var self = this;
+        if(! this.text ) {
+            this.text = TextRenderer.render(this.font, "Loading...");
+            this.scene.add(this.text);
+        }
         if (game.loader.done(this.getAssets())) {
             game.operations.push(function() {
                 return game.setState(self.nextState);
             });
         }
+    };
+
+    LoaderState.prototype.render = function(game) {
         game.renderer.clear();
-        return game.renderer.render(this.scene, this.camera);
+        game.renderer.render(this.scene, this.camera);
     };
 
     return LoaderState;
